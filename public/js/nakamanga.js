ce5b9cc91b0c728df5d53b509a86d5a593115255
iqbal090202 <ialamsyah92@gmail.com>
2019-09-07 13:40:09 +0700

    menambahkan asset di public

---
+++ public/js/nakamanga.js
@@ -0,0 +1,141 @@
+$(function () {
+
+    // $('#tombolSidebar').on('click', function () {
+    //     $('#sidebar').toggleClass('sidebar-active');
+    //     $('#content').toggleClass('content-active');
+    // });
+
+    // $('#home').on('click', function () {
+    //     $(this).toggleClass('active');
+    // });
+
+    // $('.nav .master').on('click', function () {
+    //     $('.nav .master ul').slideToggle();
+    // });
+
+    // $('.nav .data').on('click', function () {
+    //     $('.nav .data ul').slideToggle();
+    // });
+
+    // $('.nav .setup').on('click', function () {
+    //     $('.nav .setup ul').slideToggle();
+    // });
+
+
+
+    // // Jenis Buku
+    // $('.tambahJenis').on('click', function () {
+    //     $('#form').attr('class', 'formTambah');
+    //     $('#judulModal').html('Tambah');
+    //     $('.modal-footer button[type=submit]').html('Tambah');
+    //     $('.modal-body form').attr('action', 'http://localhost/iaperpus/public/JenisBuku/tambah');
+    //     $('#jenis').val('');
+    // });
+
+    // $('.editJenis').on('click', function () {
+    //     $('#form').attr('class', 'formEdit');
+    //     $('#judulModal').html('Edit');
+    //     $('.modal-footer button[type=submit]').html('Edit');
+    //     $('.modal-body form').attr('action', 'http://localhost/iaperpus/public/JenisBuku/edit');
+
+    //     const id = $(this).data('id');
+
+    //     $.ajax({
+    //         url: 'http://localhost/iaperpus/public/JenisBuku/getEdit',
+    //         data: {
+    //             id: id
+    //         },
+    //         method: 'post',
+    //         dataType: 'json',
+    //         success: function (data) {
+    //             $('#id').val(data.id);
+    //             $('#jenis').val(data.jenis);
+    //         }
+    //     });
+
+    // });
+
+    // // Anggota
+    // $('.tambahAnggota').on('click', function () {
+    //     $('#form').attr('class', 'formTambah');
+    //     $('#judulModal').html('Tambah');
+    //     $('.modal-footer button[type=submit]').html('Tambah');
+    //     $('.modal-body form').attr('action', 'http://localhost/iaperpus/public/Anggota/tambah');
+    //     $('#noagt').val('');
+    //     $('#namaagt').val('');
+    //     $('#alamat').val('');
+    //     $('#kota').val('');
+    //     $('#telp').val('');
+    // });
+
+    // $('.editAnggota').on('click', function () {
+    //     $('#form').attr('class', 'formEdit');
+    //     $('#judulModal').html('Edit');
+    //     $('.modal-footer button[type=submit]').html('Edit');
+    //     $('.modal-body form').attr('action', 'http://localhost/iaperpus/public/Anggota/edit');
+
+    //     const id = $(this).data('id');
+
+    //     $.ajax({
+    //         url: 'http://localhost/iaperpus/public/Anggota/getEdit',
+    //         data: {
+    //             id: id
+    //         },
+    //         method: 'post',
+    //         dataType: 'json',
+    //         success: function (data) {
+    //             $('#id').val(data.id);
+    //             $('#noagt').val(data.noagt);
+    //             $('#namaagt').val(data.namaagt);
+    //             $('#alamat').val(data.alamat);
+    //             $('#kota').val(data.kota);
+    //             $('#telp').val(data.telp);
+    //         }
+    //     });
+
+    // });
+
+    // // Buku
+    // $('.tambahBuku').on('click', function () {
+    //     $('#form').attr('class', 'formTambah');
+    //     $('#judulModal').html('Tambah');
+    //     $('.modal-footer button[type=submit]').html('Tambah');
+    //     $('.card-body form').attr('action', 'http://localhost/iaperpus/public/buku/tambah');
+    //     $('#judul').val('');
+    //     $('#pengarang').val('');
+    //     $('#thnterbit').val('');
+    //     $('#penerbit').val('');
+    //     $('#harga').val('');
+    //     $('#tersedia').val('');
+    // });
+
+    // $('.editBuku').on('click', function () {
+    //     $('#form').attr('class', 'formEdit');
+    //     $('#judulModal').html('Edit');
+    //     $('.modal-footer button[type=submit]').html('Edit');
+    //     $('.card-body form').attr('action', 'http://localhost/iaperpus/public/buku/edit');
+
+    //     const id = $(this).data('id');
+    //     console.log(id);
+    //     $.ajax({
+    //         url: 'http://localhost/iaperpus/public/buku/getEdit',
+    //         data: {
+    //             id: id
+    //         },
+    //         method: 'post',
+    //         dataType: 'json',
+    //         success: function (data) {
+    //             $('#id').val(data.id);
+    //             $('#judul').val(data.judul);
+    //             $('#pengarang').val(data.pengarang);
+    //             $('#thnterbit').val(data.thnterbit);
+    //             $('#penerbit').val(data.penerbit);
+    //             $('#harga').val(data.harga);
+    //             $('#tersedia').val(data.tersedia);
+    //         }
+    //     });
+
+    // });
+
+
+});
