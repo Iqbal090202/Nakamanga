e615695c63c152d35e05234d9d79a5a10264ec68
iqbal090202 <ialamsyah92@gmail.com>
2019-10-22 16:27:01 +0700

    coba api untuk di mobile

--- app/Http/Controllers/API/KomikApiController.php
+++ app/Http/Controllers/API/KomikApiController.php
@@ -27,7 +27,7 @@ class KomikApiController extends Controller
     }
 
     public function search($judul) {
-        $komik = komik::where('judul_komik', 'like', "%{$judul}%");
+        $komik = komik::where('judul_komik', 'like', "%{$judul}%")->get();
         if($komik->count() > 0) {
             return response()->json([
                 'message' => 'berhasil',
@@ -38,69 +38,4 @@ class KomikApiController extends Controller
         }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
